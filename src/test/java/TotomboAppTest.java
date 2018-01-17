e7a6c5b65e6634ba619479c0812f030130fddd5f
mherbaghinyan <mbaghinyan@gmail.com>
2018-01-17 22:59:03 +0400

    added test case get api

---
+++ src/test/java/TotomboAppTest.java
@@ -0,0 +1,23 @@
+import static io.restassured.RestAssured.get;
+import static org.hamcrest.Matchers.equalTo;
+
+import com.totombo.jooby.TotomboApp;
+import org.jooby.test.JoobyRule;
+import org.junit.ClassRule;
+import org.junit.Test;
+
+/**
+ * Created by Gebruiker on 1/17/2018.
+ */
+public class TotomboAppTest {
+
+    @ClassRule
+    public static JoobyRule app = new JoobyRule(new TotomboApp());
+
+    @Test
+    public void getTotomboApi() {
+
+        get("/api").then().assertThat().body(equalTo("Totombo API"))
+                .statusCode(200).contentType("text/html;charset=UTF-8");
+    }
+}
