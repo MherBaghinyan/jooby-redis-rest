24be60e376d3dd32c57aef25d5ef1fb0d612769d
Mher Baghinyan <mbaghinyan@gmail.com>
2018-01-18 20:47:29 +0400

    Update TotomboController.java

--- src/main/java/com/totombo/jooby/TotomboController.java
+++ src/main/java/com/totombo/jooby/TotomboController.java
@@ -1,7 +1,31 @@
 package com.totombo.jooby;
 
+import org.jooby.mvc.GET;
+import org.jooby.mvc.POST;
+import org.jooby.mvc.Path;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
 /**
  * Created by Gebruiker on 1/17/2018.
  */
+@Path("/api")
 public class TotomboController {
+
+/*
+http://www.baeldung.com/jooby
+https://github.com/eugenp/tutorials/blob/master/jooby/src/test/java/com/baeldung/jooby/AppTest.java
+
+
+http://jooby.org/guides/hello-restful/
+https://github.com/jooby-project/greeting/blob/master/src/main/java/greeting/Greetings.java
+ */
+
+    @Path({"/count"})
+    @GET
+    public String incrementCounter() {
+
+        AtomicInteger count = new AtomicInteger(0);
+        return "count: " + count.incrementAndGet();
+    }
 }
